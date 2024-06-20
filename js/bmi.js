7977d0192f4cbb00e431ec2686ba642e18d33756
Kaoru <kaoru@gmail.com>
2024-06-21 00:36:43 +0800

    新增清除的click按鈕

--- js/bmi.js
+++ js/bmi.js
@@ -1,11 +1,16 @@
 const nameEl = document.querySelector("#name");
 const heightEl = document.querySelector("#height");
 const weightEl = document.querySelector("#weight");
+const bmiEl = document.querySelector("#bmi");
+const commentEl = document.querySelector("#comment");
 
-console.log(nameEl, weightEl, heightEl);
+
+
+console.log(nameEl, weightEl, heightEl, bmiEl, commentEl);
 
 
 function calcBmi() {
+    // 計算BMI
     let height = heightEl.value;
     let weight = weightEl.value;
 
@@ -15,7 +20,27 @@ function calcBmi() {
     }
 
     let bmi = getBmi(height, weight)
-    console.log(bmi);
+    bmiEl.innerText = `BMI:${bmi}`;
+
+
+
+    // 取得評語
+    let comment;
+    if (bmi < 18.5) {
+        comment = `過輕`;
+    } else if (bmi < 24) {
+        comment = `正常`;
+    } else if (bmi < 27) {
+        comment = `過重`;
+    } else if (bmi < 30) {
+        comment = `輕度肥胖`;
+    } else if (bmi < 35) {
+        comment = `中度肥胖`;
+    } else {
+        comment = `重度肥胖`;
+    }
+    console.log(bmi, comment);
+    commentEl.innerText = `${comment}`;
 }
 
 
@@ -23,11 +48,22 @@ function getBmi(height, weight) {
     let bmi = weight / (height / 100) ** 2;
 
     if (isNaN(bmi)) {
-        return "數值錯誤"
+        return "數值錯誤";
     }
-    return bmi.toFixed(2)
+    return bmi.toFixed(2);
 }
 
 calcBmi();
 
 
+
+function getCls() {
+    // input的取值與設定
+    nameEl.value = "";
+    heightEl.value = "";
+    weightEl.value = "";
+
+    // 內文部分
+    bmiEl.innerText = "";
+    commentEl.innerText = "";
+}
